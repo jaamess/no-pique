622afd4f7a82bbd109c3d52944bdb3ea4ba3df56
Thiago Azevedo <thiagozevedo3@gmail.com>
2019-12-29 22:27:00 -0300

    guildMemberAdd event

---
+++ monitors/guildMemberAdd.js
@@ -0,0 +1,19 @@
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+
+    constructor(...args) {
+        super(...args, {
+            enabled: true,
+            once: false
+        });
+    }
+
+    async run(member) {
+        const welcomeChannel = member.guild.channels.get('639324481227194377');
+        welcomeChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Novo membro: ${member.displayName}.`);
+        
+        return member.roles.add('653059521778024468');
+    }
+
+};
