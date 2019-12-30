dd508b17ac211ea757514b13cc3d2bb73fdfd59d
Thiago Azevedo <thiagozevedo3@gmail.com>
2019-12-29 22:29:01 -0300

    fix

--- monitors/guildMemberAdd.js
+++ monitors/guildMemberAdd.js
@@ -4,12 +4,14 @@ module.exports = class extends Event {
 
     constructor(...args) {
         super(...args, {
+            name: 'guildMemberAdd',
             enabled: true,
             once: false
         });
     }
 
     async run(member) {
+        console.log
         const welcomeChannel = member.guild.channels.get('639324481227194377');
         welcomeChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Novo membro: ${member.displayName}.`);
         
