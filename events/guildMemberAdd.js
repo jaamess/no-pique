f08373795177ced966b22954a2ba99806b9aec9b
Thiago Azevedo <thiagozevedo3@gmail.com>
2019-12-29 22:32:11 -0300

    fix

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -13,7 +13,7 @@ module.exports = class extends Event {
     async run(member) {
         console.log
         const welcomeChannel = member.guild.channels.get('639324481227194377');
-        welcomeChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Novo membro: ${member.displayName}**`);
+        welcomeChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Novo membro: <@${member.user.id}>**`);
         
         return member.roles.add('653059521778024468');
     }
