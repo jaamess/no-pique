6d8d80054ecc959fea14d87e36db536b9d12acad
Thiago Azevedo <thiagozevedo3@gmail.com>
2020-01-01 19:39:26 -0300

    oops

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -11,11 +11,10 @@ module.exports = class extends Event {
     }
 
     async run(member) {
-        console.log
         const welcomeChannel = member.guild.channels.get('639324481227194377');
         const greetChannel = member.guild.channels.get('639318834989498378');
         welcomeChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Novo membro: <@${member.user.id}>**`);
-        greetChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Bem vindo <@${member.user.id}>! Leia as <#639319056343760918> e sinta-se em casa <a:pikachu_legal:644005135093202954>**`);
+        greetChannel.send(`<a:N1:657447252440252416><a:P1:657447252301840399>  **|  Bem vindo(a) <@${member.user.id}>! Leia as <#639319056343760918> e sinta-se em casa <a:pikachu_legal:644005135093202954>**`);
 
         return member.roles.add('653059521778024468');
     }
